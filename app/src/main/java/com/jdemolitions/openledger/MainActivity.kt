428d2622b0cb83b32ac30ae6750a585135b8bb11
Jeff <jefferson.zapata.sierra@gmail.com>
2018-09-08 17:43:34 -0500

    Add DrawerLayout and main navigation

--- app/src/main/java/com/jdemolitions/openledger/MainActivity.kt
+++ app/src/main/java/com/jdemolitions/openledger/MainActivity.kt
@@ -1,18 +1,45 @@
 package com.jdemolitions.openledger
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.jdemolitions.openledger.ui.main.MainFragment
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.jdemolitions.openledger.databinding.MainActivityBinding
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+
+        val binding: MainActivityBinding = DataBindingUtil.setContentView(this, R.layout.main_activity)
+        drawerLayout = binding.drawerLayout
+        val navController = Navigation.findNavController(this, R.id.main_nav_host_fragment)
+
+        setSupportActionBar(binding.toolbar)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        binding.navigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+                drawerLayout,
+                Navigation.findNavController(this, R.id.main_nav_host_fragment)
+        )
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
 
